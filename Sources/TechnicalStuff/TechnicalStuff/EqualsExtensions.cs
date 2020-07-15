dc41844662340845e41d9b9b092e8071d14e7a8d
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2020-07-15 18:01:34 +0200

    Missing EqualsExtensions added

---
+++ Sources/TechnicalStuff/TechnicalStuff/EqualsExtensions.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace MyCompany.Crm.TechnicalStuff
+{
+    public static class EqualsExtensions
+    {
+        public static bool HasSameItemsAs<T>(this ImmutableArray<T> array, ImmutableArray<T> other)
+        {
+            var length = array.Length;
+            if (other.Length != length) return false;
+            var counts = new Dictionary<T, int>(length);
+            for (var i = 0; i < length; i++)
+            {
+                var item = array[i];
+                if (counts.ContainsKey(item))
+                    counts[item]++;
+                else
+                    counts.Add(item, 1);
+            }
+            for (var i = 0; i < length; i++)
+            {
+                var item = other[i];
+                if (counts.TryGetValue(item, out var count))
+                {
+                    if (count == 1)
+                        counts.Remove(item);
+                    else
+                        counts[item]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return counts.Count == 0;
+        }
+
+        public static bool HasSameItemsAs<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            IEnumerable<KeyValuePair<TKey, TValue>> other)
+        {
+            var count = 0;
+            foreach (var (key, otherValue) in other)
+            {
+                if (!dictionary.TryGetValue(key, out var value) || !otherValue.Equals(value))
+                    return false;
+                count++;
+            }
+            return dictionary.Count == count;
+        }
+    }
+}
---
+++ Sources/TechnicalStuff/TechnicalStuff/HashCodeExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace MyCompany.Crm.TechnicalStuff
+{
+    public static class HashCodeExtensions
+    {
+        public static HashCode CombineWith<T>(this HashCode hashCode, T value)
+        {
+            hashCode.Add(value);
+            return hashCode;
+        }
+        
+        public static HashCode CombineWith<T>(this HashCode hashCode, ImmutableArray<T> array)
+        {
+            var length = array.Length;
+            for (var i = 0; i < length; i++) hashCode.Add(array[i]);
+            return hashCode;
+        }
+        
+        public static HashCode CombineWith<T>(this HashCode hashCode, IEnumerable<T> enumerable)
+        {
+            foreach (var item in enumerable) hashCode.Add(item);
+            return hashCode;
+        }
+    }
+}
