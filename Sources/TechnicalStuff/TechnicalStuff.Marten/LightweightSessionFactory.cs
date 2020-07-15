4975fa173fb4424ce63e86d724d41e7f8c520a30
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2020-07-15 18:06:40 +0200

    TechnicalStuff.Marten.csproj added

---
+++ Sources/TechnicalStuff/TechnicalStuff.Marten/LightweightSessionFactory.cs
@@ -0,0 +1,25 @@
+using System.Data;
+using JetBrains.Annotations;
+using Marten;
+using Marten.Services;
+
+namespace MyCompany.Crm.TechnicalStuff.Marten
+{
+    [UsedImplicitly]
+    public class LightweightSessionFactory : ISessionFactory
+    {
+        private readonly IDocumentStore _store;
+
+        public LightweightSessionFactory(IDocumentStore store) => _store = store;
+
+        public IQuerySession QuerySession() => _store.QuerySession();
+
+        public IDocumentSession OpenSession() => _store.OpenSession(new SessionOptions
+        {
+            Tracking = DocumentTracking.None,
+            IsolationLevel = IsolationLevel.ReadCommitted,
+            EnlistInAmbientTransactionScope = true,
+            OwnsTransactionLifecycle = false
+        });
+    }
+}
