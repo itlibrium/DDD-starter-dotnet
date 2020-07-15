18fb16de76a1ff194cc2c33519a5a86c6afb3729
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2020-07-15 18:07:29 +0200

    SystemClock time resolution adjusted

--- Sources/Sales/Sales.Domain/Time/SystemClock.cs
+++ Sources/Sales/Sales.Domain/Time/SystemClock.cs
@@ -4,6 +4,13 @@ namespace MyCompany.Crm.Sales.Time
 {
     internal class SystemClock : Clock
     {
-        public DateTime Now => DateTime.UtcNow;
+        public DateTime Now
+        {
+            get
+            {
+                var now = DateTime.UtcNow;
+                return new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second, now.Millisecond);
+            }
+        }
     }
 }
