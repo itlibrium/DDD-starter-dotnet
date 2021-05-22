2032a39801abb6bd75543d14ff40c512ede11c23
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2021-05-22 14:11:46 +0200

    Handle method for non generic CommandHandler

--- Sources/TechnicalStuff/TechnicalStuff.UseCases.Contracts/CommandHandler.cs
+++ Sources/TechnicalStuff/TechnicalStuff.UseCases.Contracts/CommandHandler.cs
@@ -2,17 +2,29 @@ using System.Threading.Tasks;
 
 namespace MyCompany.Crm.TechnicalStuff.UseCases
 {
-    public interface CommandHandler { }
-    
-    public interface CommandHandler<in TCommand> : CommandHandler
-        where TCommand : struct, Command
+    public interface CommandHandler
     {
+        Task Handle(Command command);
+    }
+
+    public interface CommandHandler<in TCommand> : CommandHandler
+        where TCommand : Command
+    {
+        Task CommandHandler.Handle(Command command)
+        {
+            if (!(command is TCommand tCommand))
+                throw new DesignError($"{command.GetType().Name} in incompatible with {GetType().Name}");
+            return Handle(tCommand);
+        }
+
         Task Handle(TCommand command);
     }
-    
-    public interface CommandHandler<in TCommand, TResult> : CommandHandler
-        where TCommand : struct, Command
+
+    public interface CommandHandler<in TCommand, TResult> : CommandHandler<TCommand>
+        where TCommand : Command
     {
-        Task<TResult> Handle(TCommand command);
+        Task CommandHandler<TCommand>.Handle(TCommand command) => Handle(command);
+
+        new Task<TResult> Handle(TCommand command);
     }
 }
