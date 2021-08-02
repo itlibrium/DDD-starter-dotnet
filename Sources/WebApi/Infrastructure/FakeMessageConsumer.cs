73c0a139906a9890197c40309a38f683374b87c2
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2021-08-02 15:08:30 +0200

    Debug logging in FakeMessageConsumer

--- Sources/WebApi/Infrastructure/FakeMessageConsumer.cs
+++ Sources/WebApi/Infrastructure/FakeMessageConsumer.cs
@@ -3,7 +3,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MyCompany.Crm.Sales;
+using MyCompany.Crm.TechnicalStuff;
 using MyCompany.Crm.TechnicalStuff.UseCases;
 
 namespace MyCompany.Crm.Infrastructure
@@ -12,11 +14,14 @@ namespace MyCompany.Crm.Infrastructure
     {
         private readonly FakeMessageBroker _broker;
         private readonly IServiceScopeFactory _scopeFactory;
+        private readonly ILogger<FakeMessageConsumer> _logger;
 
-        public FakeMessageConsumer(FakeMessageBroker broker, IServiceScopeFactory scopeFactory)
+        public FakeMessageConsumer(FakeMessageBroker broker, IServiceScopeFactory scopeFactory,
+            ILogger<FakeMessageConsumer> logger)
         {
             _broker = broker;
             _scopeFactory = scopeFactory;
+            _logger = logger;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken) => Task.WhenAll(
@@ -28,9 +33,18 @@ namespace MyCompany.Crm.Infrastructure
             while (!cancellationToken.IsCancellationRequested)
             {
                 var command = await _broker.ReadCommand(cancellationToken);
+                _logger.LogDebug("Handling command started. Command type: {CommandType}", command.GetType().Name);
                 using var scope = _scopeFactory.CreateScope();
                 var handler = (MessageHandler) scope.ServiceProvider.GetService(CreateCommandHandlerTypeFor(command));
-                await handler.Handle(command);
+                try
+                {
+                    await handler.Handle(command);
+                }
+                catch (DomainError e)
+                {
+                    _logger.LogError(e, "Domain error");
+                }
+                _logger.LogDebug("Handling command finished. Command type: {CommandType}", command.GetType().Name);
             }
         }
 
@@ -42,10 +56,19 @@ namespace MyCompany.Crm.Infrastructure
             while (!cancellationToken.IsCancellationRequested)
             {
                 var domainEvent = await _broker.ReadEvent(cancellationToken);
+                _logger.LogDebug("Handling event started. Command type: {EventType}", domainEvent.GetType().Name);
                 using var scope = _scopeFactory.CreateScope();
                 var handler =
                     (MessageHandler) scope.ServiceProvider.GetService(CreateEventHandlerTypeFor(domainEvent));
-                await handler.Handle(domainEvent);
+                try
+                {
+                    await handler.Handle(domainEvent);
+                }
+                catch (DomainError e)
+                {
+                    _logger.LogError(e, "Domain error");
+                }
+                _logger.LogDebug("Handling event finished. Command type: {EventType}", domainEvent.GetType().Name);
             }
         }
 
