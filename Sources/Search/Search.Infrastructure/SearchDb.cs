64f93efeb435e964ec58919a9963471b864bf6a0
Marcin Markowski <marcin.markowski@itlibrium.com>
2023-09-26 19:34:49 +0200

    search database (P3) annotation

--- Sources/Search/Search.Infrastructure/SearchDb.cs
+++ Sources/Search/Search.Infrastructure/SearchDb.cs
@@ -1,7 +1,9 @@
 using JetBrains.Annotations;
+using P3Model.Annotations.Technology;
 
 namespace MyCompany.ECommerce.Search;
 
+[Database("Search", ClusterName = "Elastic")]
 [UsedImplicitly]
 internal class SearchDb
 {
