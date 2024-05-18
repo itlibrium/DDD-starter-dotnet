68c64c1bcd912f1c99ab97f0882bd5edf0552993
Marcin Markowski <marcin.markowski@itlibrium.com>
2024-05-18 12:37:07 +0200

    typo fix

--- Sources/Sales/Sales.Adapters/Orders/InPLaceOrderEventsOutbox.cs
+++ Sources/Sales/Sales.Adapters/Orders/InPLaceOrderEventsOutbox.cs
@@ -4,11 +4,11 @@ using MyCompany.ECommerce.TechnicalStuff.ProcessModel;
 
 namespace MyCompany.ECommerce.Sales.Orders
 {
-    public class InPLaceOrderEventsOutbox : InPlaceTransactionalOutbox<OrderEvent>, OrderEventsOutbox
+    public class InPlaceOrderEventsOutbox : InPlaceTransactionalOutbox<OrderEvent>, OrderEventsOutbox
     {
         [SuppressMessage("ReSharper", "SuggestBaseTypeForParameterInConstructor",
             Justification = "Required by DI container")]
-        public InPLaceOrderEventsOutbox(TransactionalOutboxes outboxes, TransactionalOutboxRepository repository,
+        public InPlaceOrderEventsOutbox(TransactionalOutboxes outboxes, TransactionalOutboxRepository repository,
             MessageTypes messageTypes)
             : base(outboxes, repository, messageTypes) { }
 
